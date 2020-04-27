027bea33a8d42fc042043984108df26bf0c26beb
mashazavolnyuk <mashazavolnyuk@yandex.ru>
2020-04-27 16:53:57 +0300

    Write data to firebase. Rules in firebase {   "rules": {     ".read": true,     ".write": true   } }

---
+++ app/src/main/java/com/kishynskaya/usingfirebase/Film.kt
@@ -0,0 +1,2 @@
+package com.kishynskaya.usingfirebase
+data class Film (var id:String, var title: String)
--- app/src/main/java/com/kishynskaya/usingfirebase/MainActivity.kt
+++ app/src/main/java/com/kishynskaya/usingfirebase/MainActivity.kt
@@ -5,19 +5,41 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var dataFirebase: FirebaseDatabase
+    lateinit var dataLocal: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        }
+        dataFirebase = FirebaseDatabase.getInstance()
+        dataLocal = dataFirebase.reference.child("NewData")
+    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.save -> {
+                save()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun save() {
+        var film = Film(idText.text.toString(), valueText.text.toString())
+        dataLocal.push().setValue(film)
     }
 }
